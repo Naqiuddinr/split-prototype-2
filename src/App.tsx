6d1d1f1e87ee1f0189c8b6fc3373b0d56bb40510
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-11-02 11:17:21 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
 
   return (
     <main>
+      <h1>This is staging</h1>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
