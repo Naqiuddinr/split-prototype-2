dc43302630cc07156d39e5773d66673137d71719
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-11-07 22:41:51 +0800

    chore:resolve pipeline error

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../components/auth/useAuth";
-import { PasswordInput } from "../../components/form-input/passwordInput";
+import { PasswordInput } from "../../components/form-input/passwordInput.tsx";
 
 export default function Login() {
 
