fee19bf69f05eaa304866c146b4d720b5a38cf16
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-11-07 22:52:22 +0800

    chore:resolve pipeline error - changes in tsconfig

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../components/auth/useAuth";
-import { PasswordInput } from "../../components/form-input/passwordInput.tsx";
+import { PasswordInput } from "../../components/form-input/PasswordInput";
 
 export default function Login() {
 
