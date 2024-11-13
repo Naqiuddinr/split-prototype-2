a1d6ba6ee3355dc0c72dd1076ac8f3eec058d533
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-11-13 22:25:28 +0800

    fix navbar, moved logout button

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,40 +1,81 @@
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import {
+    AppBar,
+    Box,
+    // Button,
+    Drawer,
+    IconButton,
+    Toolbar,
+    Typography
+} from '@mui/material';
+// import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useState } from 'react';
+import { useAuth } from '../auth/useAuth';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Navbar() {
 
-    const [showMenu, setShowMenu] = useState(false);
+    const [showDrawer, setShowDrawer] = useState(false);
 
     return (
         <>
-            <header className="bg-black h-14 m-4 py-2 px-4 flex items-center rounded-full justify-between relative z-10">
-                <div>
-                    <h1 className="text-white">Tong2</h1>
-                </div>
-                <div>
-                    {showMenu ? (
-                        <div>
-                            <CloseOutlinedIcon className="text-white cursor-pointer" onClick={() => setShowMenu(false)} />
-                        </div>
-                    ) : (
-                        <MenuOutlinedIcon className="text-white cursor-pointer" onClick={() => setShowMenu(true)} />
-                    )}
-                </div>
-            </header>
-
-            <div
-                className={`bg-black text-white transition-all duration-500 overflow-hidden`}
-                style={{
-                    height: showMenu ? '100px' : '0',
-                    margin: '0 1rem', // Make sure the hidden div has the same margin as the header
-                    padding: '1rem', // If you want padding inside the div
-                }}
-            >
-                hello
-            </div>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: "none", borderBottom: "1px solid #e5e7eb" }}>
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "black" }}>
+                            Tong2
+                        </Typography>
+                        <IconButton
+                            size="large"
+                            aria-label="menu"
+                            onClick={() => setShowDrawer(!showDrawer)}
+                        >
+                            <AccountCircleIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <SettingDrawer showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
+            {/* <SwipeableDrawer onClose={ } onOpen={ } /> */}
         </>
+    )
+}
 
+interface SettingDrawerProps {
+    showDrawer: boolean;
+    setShowDrawer: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
+function SettingDrawer({ showDrawer, setShowDrawer }: SettingDrawerProps) {
+
+    const { userSignout } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleSignout = async () => {
+
+        await userSignout();
+        navigate("/login");
+
+    };
+
+    return (
+        <>
+            <Drawer
+                anchor='bottom'
+                open={showDrawer}
+                onClose={() => setShowDrawer(false)}
+            >
+                <Box sx={{ width: 'auto', height: '20vh' }} className="flex justify-center items-center">
+                    <button
+                        onClick={handleSignout}
+                        className="w-1/2 rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    >
+                        Logout
+                    </button>
+                </Box>
+            </Drawer>
+        </>
     )
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,25 +1,15 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../components/auth/useAuth";
-import { Button } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../../components/auth/useAuth";
+// import { Button } from "@mui/material";
 import Navbar from "../../components/navigation/Navbar.tsx"
 
 export default function Dashboard() {
 
-    const { userSignout } = useAuth();
 
-    const navigate = useNavigate();
-
-    const handleSignout = async () => {
-
-        await userSignout();
-        navigate("/login");
-
-    };
 
     return (
         <>
             <Navbar />
-            <Button variant="contained" style={{ backgroundColor: "black" }} onClick={handleSignout}>Sign Out</Button>
         </>
     );
 }
